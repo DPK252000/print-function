5304a7821d198acb0fce37bf29462b0f92a34501
DPK252000 <113545795+DPK252000@users.noreply.github.com>
2022-10-02 22:57:28 +0530

    Add files via upload

---
+++ print_function.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        print(i, end="")
+
+        if __name__ == '__main__':
+           n = int(input())
+    for i in range(6,n+6):
+        print(i,end='')
